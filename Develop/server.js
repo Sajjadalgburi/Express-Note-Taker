74b0d1b43ba9eb077cb7ca742d8948f59fdf929a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-11 15:12:32 -0700

    fixed naming bugs... added get route for 404 status

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = procces.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware's for json, grabbing user data and static assets
 app.use(express.static("public"));
-app.use(experss.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// route for 404 page
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/404.html"));
-});
 
 // route for homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/homepage.html"));
+});
+
+// route for 404 page
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "public/404.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on server http://localhost:${PORT}`);
 });
