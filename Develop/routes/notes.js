71b64d86ecc27bdf5e8ed37416561a9aaee74904
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-11 15:15:49 -0700

    all the helpers i ned in the costum notes route

---
+++ Develop/routes/notes.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const notes = require("express").Router();
+const uuid = require("../helpers/uuid");
+
+// Helper functions for reading and writing to the JSON file
+const {
+  readFromFile,
+  writeToFile,
+  readAndAppend,
+} = require("../helpers/fsUtils");
+
+// Define the path to db.json
+const dbFilePath = path.join(__dirname, "../db/db.json");
