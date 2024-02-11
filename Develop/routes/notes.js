c3ad5aed51d7558178f6adb8bf51ae46f7f4d555
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-11 15:27:24 -0700

    GET method to read the db.json file and return all saved notes as JSON

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -12,4 +12,10 @@ const {
 // Define the path to db.json
 const dbFilePath = path.join(__dirname, "../db/db.json");
 
+// GET method to read the db.json file and return all saved notes as JSON
+notes.get("/", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile(dbFilePath).then((data) => res.json(JSON.parse(data)));
+});
+
 module.exports = notes;
