126c65bff1c44fd1f004a2fdbc654e2f96a96ef7
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-11 15:05:37 -0700

    addes middlewares for server

--- Develop/server.js
+++ Develop/server.js
@@ -2,3 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = procces.env.PORT || 3001;
+
+// Middleware's for json, grabbing user data and static assets
+app.use(express.static("public"));
+app.use(experss.json());
+app.use(express.urlencoded({ extended: true }));
